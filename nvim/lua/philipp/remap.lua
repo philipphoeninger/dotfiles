b2420543f985ffc8f1bc7917533147e616637c84
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2025-03-30 15:08:22 +0200

    add single line scroll remaps

--- nvim/lua/philipp/remap.lua
+++ nvim/lua/philipp/remap.lua
@@ -19,6 +19,8 @@ vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
 -- vertical scroll and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "<C-k>", "<C-e>", opts)
+vim.keymap.set("n", "<C-j>", "<C-y>", opts)
 
 -- find and center
 vim.keymap.set("n", "n", "nzzzv", opts)
@@ -33,20 +35,20 @@ vim.keymap.set("n", "<Right>", ":vertical resize -2<CR>", opts)
 -- buffers
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts)   -- close buffer
 vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
 
 -- window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split equally sized
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts)      -- split window vertically
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts)      -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts)     -- make split equally sized
 vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close split
 
 -- navigate between splits
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts) -- up
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts) -- down
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts) -- left
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts) -- right
+vim.keymap.set("n", "<S-k>", ":wincmd k<CR>", opts) -- up
+vim.keymap.set("n", "<S-j>", ":wincmd j<CR>", opts) -- down
+vim.keymap.set("n", "<S-h>", ":wincmd h<CR>", opts) -- left
+vim.keymap.set("n", "<S-l>", ":wincmd l<CR>", opts) -- right
 
 -- toggle line wraping
 vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts) -- right
@@ -66,4 +68,3 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
